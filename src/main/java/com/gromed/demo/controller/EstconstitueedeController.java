dfc866b02d47a48edbe749169a835968d6331bf5
l3miage-meassone <emerik.measson@gmail.com>
2023-01-27 16:12:45 +0100

    push controller pour créer un panier

--- src/main/java/com/gromed/demo/controller/EstconstitueedeController.java
+++ src/main/java/com/gromed/demo/controller/EstconstitueedeController.java
@@ -1,9 +1,17 @@
 package com.gromed.demo.controller;
 
+import com.gromed.demo.model.*;
+import com.gromed.demo.service.CommandeService;
+import com.gromed.demo.service.CompteService;
 import com.gromed.demo.service.EstconstitueedeService;
+import com.gromed.demo.service.PresentationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Validated
 @RestController
@@ -13,6 +21,52 @@ public class EstconstitueedeController {
     @Autowired
     private EstconstitueedeService estconstitueedeService;
 
+    @Autowired
+    private CompteService compteService;
+
+    @Autowired
+    private CommandeService commandeService;
+
+    @Autowired
+    private PresentationService presentationService;
+
+    @PostMapping("/create")
+    public Estconstitueede createCommande(@RequestBody AchatPresentation achatPresentation) {
+        if (achatPresentation == null || achatPresentation.getQuantiteCommande() == 0 || achatPresentation.getProduit() == null || achatPresentation.getIdCompte() == null) {
+            throw new ResponseStatusException(HttpStatus.PRECONDITION_FAILED, "Il manque le numéro du produit souhaité ou la quantité souhaitée ou votre identifiant");
+        }
+
+        //////////////////////////////////////
+        Commande newCommande = new Commande();
+        Optional<Compte> optionalCompte = compteService.getCompte(achatPresentation.getIdCompte());
+        if (!optionalCompte.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.PRECONDITION_FAILED, "L'identifiant de l'utilisateur est incorrect");
+        }
+        Compte reelCompte = optionalCompte.get();
+        newCommande.setCompte(reelCompte);
+        newCommande = commandeService.saveCommande(newCommande);
+        ///////////////////////////////////
+        Estconstitueede newEstConstitueede = new Estconstitueede();
+        newEstConstitueede.setCommande(newCommande);
+
+        newEstConstitueede.setQuantite(achatPresentation.getQuantiteCommande());
+
+        EstconstitueedeId newEstConsitueeDeId = new EstconstitueedeId();
+        newEstConsitueeDeId.setIdcommande(newCommande.getId());
+        newEstConsitueeDeId.setCodecip7(achatPresentation.getProduit());
+        newEstConstitueede.setId(newEstConsitueeDeId);
+
+        Optional <Presentation> optionalPresentation = presentationService.getPresentation(achatPresentation.getProduit());
+        if(!optionalPresentation.isPresent()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Produit non trouvé");
+        }
+        Presentation reelPresentation = optionalPresentation.get();
+        newEstConstitueede.setPresentation(reelPresentation);
+        newEstConstitueede.setTerminer(false);
+
+        newEstConstitueede = estconstitueedeService.saveEstconstitueede(newEstConstitueede);
+        return newEstConstitueede;
+    }
 
 
 }
---
+++ src/main/java/com/gromed/demo/model/AchatPresentation.java
@@ -0,0 +1,22 @@
+package com.gromed.demo.model;
+
+public class AchatPresentation {
+
+    private Long produit;
+
+    private int quantiteCommande;
+
+    public Long idCompte;
+
+    public Long getProduit(){
+        return produit;
+    }
+
+    public int getQuantiteCommande(){
+        return quantiteCommande;
+    }
+
+    public Long getIdCompte(){
+        return idCompte;
+    }
+}
