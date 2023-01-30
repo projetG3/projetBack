f4542a0ff0b21956e4cc5ce7e69582fa199f649d
l3miage-fatelali <iris.fatela-le-roy@etu.univ-grenoble-alpes.fr>
2023-01-30 15:11:45 +0100

    commentaire commande

--- src/main/java/com/gromed/demo/service/CommandeService.java
+++ src/main/java/com/gromed/demo/service/CommandeService.java
@@ -50,61 +50,35 @@ public class CommandeService {
         List<Estconstitueede> estconstitueedesNoStocks = new ArrayList<>();
         List<Estconstitueede> estconstitueedes = commande.get().getEstconstitueedes();
 
-        Integer quantite;
         Long montantTotal = Long.valueOf(0);
-
-        /*Connection con = DbConnection.getConnection();
-        con.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
-        String sql;
-        PreparedStatement statement;
-         */
 
         for (Estconstitueede estconstitueede : estconstitueedes) {
             Presentation presentation = estconstitueede.getPresentation();
             if (estconstitueede.getQuantite() > presentation.getQuantitedispo()) {
                 estconstitueedesNoStocks.add(estconstitueede);
-                quantite = estconstitueede.getQuantite() - presentation.getQuantitedispo();
 
                 Estlivree estlivree = new Estlivree();
-                System.out.println(commande.get().getId() + "  " + estconstitueede.getPresentation().getId());
-                System.out.println(LocalDate.now() + "  " + Long.valueOf(presentation.getQuantitedispo()));
                 estlivree.setIdcommande(commande.get());
                 estlivree.setPresentation(estconstitueede.getPresentation());
                 estlivree.setStatus("envoyer");
                 estlivree.setDateenvoi(LocalDate.now());
                 estlivree.setQuantite(presentation.getQuantitedispo());
                 try{
+                    System.out.println("try est livree");
                     estlivreeService.saveEstlivree(estlivree);
                 }catch (Exception e){
+                    System.out.println("catch est livree");
                     throw e;
                 }
-                /*sql = "INSERT INTO estlivree (idcommande, codecip7, status, dateenvoi, quantite) VALUES (?, ?, ?, ?, ?)";
-                statement = con.prepareStatement(sql);
-                statement.setLong(1, commande.get().getId());
-                statement.setLong(2, estconstitueede.getPresentation().getId());
-                statement.setString(3, "envoyer");
-                statement.setDate(4, Date.valueOf(LocalDate.now()));
-                statement.setInt(5, presentation.getQuantitedispo());
-                statement.executeUpdate();
-                sql = "UPDATE estconstitueede SET quantite = ? WHERE id = ?";
-                statement = con.prepareStatement(sql);
-                statement.setInt(1, quantite);
-                statement.setLong(2, estconstitueede.getId());
-                statement.executeUpdate();
-                */
 
                 presentation.setQuantitedispo(0);
                 try{
+                    System.out.println("try presentation");
                     presentationService.savePresentation(presentation);
                 }catch (Exception e){
+                    System.out.println("catch presentation");
                     throw e;
                 }
-                /*sql = "UPDATE presentation SET quantitedispo = ? WHERE codecip7 = ?";
-                statement = con.prepareStatement(sql);
-                statement.setInt(1, 0);
-                statement.setLong(2, presentation.getId());
-                statement.executeUpdate();
-                 */
 
             } else {
                 Estlivree estlivree = new Estlivree();
@@ -114,46 +88,30 @@ public class CommandeService {
                 estlivree.setDateenvoi(LocalDate.now());
                 estlivree.setQuantite(estconstitueede.getQuantite());
                 try{
+                    System.out.println("try est livree");
                     estlivreeService.saveEstlivree(estlivree);
                 }catch (Exception e){
+                    System.out.println("catch est livree");
                     throw e;
                 }
-                /*sql = "INSERT INTO estlivree (idcommande, codecip7, status, dateenvoi, quantite) VALUES (?, ?, ?, ?, ?)";
-                statement = con.prepareStatement(sql);
-                statement.setLong(1, commande.get().getId());
-                statement.setLong(2, estconstitueede.getPresentation().getId());
-                statement.setString(3, "envoyer");
-                statement.setDate(4, Date.valueOf(LocalDate.now()));
-                statement.setInt(5, estconstitueede.getQuantite());
-                statement.executeUpdate();
-                 */
 
                 presentation.setQuantitedispo(presentation.getQuantitedispo()-estconstitueede.getQuantite());
                 try {
+                    System.out.println("try presentation");
                     presentationService.savePresentation(presentation);
                 }catch (Exception e){
+                    System.out.println("catch presentation");
                     throw e;
                 }
-                /*sql = "UPDATE presentation SET quantitedispo = ? WHERE codecip7 = ?";
-                statement = con.prepareStatement(sql);
-                statement.setInt(1, presentation.getQuantitedispo()-estconstitueede.getQuantite());
-                statement.setLong(2, presentation.getId());
-                statement.executeUpdate();
-                 */
 
                 estconstitueede.setTerminer(true);
                 try {
+                    System.out.println("try estconstituteede");
                     estconstitueedeService.saveEstconstitueede(estconstitueede);
                 }catch (Exception e){
+                    System.out.println("catch estconstituteede");
                     throw e;
                 }
-                /*sql = "UPDATE estconstitueede SET quantite = ?, terminer = ? WHERE id = ?";
-                statement = con.prepareStatement(sql);
-                statement.setInt(1, 0);
-                statement.setBoolean(2, true);
-                statement.setLong(3, estconstitueede.getId());
-                statement.executeUpdate();
-                 */
             }
             montantTotal+=presentation.getPrix();
         }
@@ -165,18 +123,14 @@ public class CommandeService {
             commande.get().setDatefacture(LocalDate.now());
             commande.get().setStatusfacture(true);
             try {
+                System.out.println("try commande");
                 saveCommande(commande.get());
             }catch (Exception e){
+                System.out.println("catch commande");
                 throw e;
             }
-            /*sql = "UPDATE commande SET status = ? WHERE idcommande = ?";
-            statement = con.prepareStatement(sql);
-            statement.setString(1, "terminer");
-            statement.setLong(2, commande.get().getId());
-            statement.executeUpdate();
-             */
         }
-
+        estconstitueedesNoStocks.forEach(s->System.out.println(s.getId()));
         return estconstitueedesNoStocks;
     }
 }
